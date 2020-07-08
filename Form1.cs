ca5481be19d2a7804e820c7bc685cc7191ce3fb7
MrFreex <33759569+MrFreex@users.noreply.github.com>
2020-07-08 16:38:59 +0200

    Update 1.2

--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -35,8 +36,14 @@ namespace RgbConverter
             } else {
                 string[] splittedInput = input.Split('.');
                 if (splittedInput.Count() == 2) {
-                    float inputNum = float.Parse(string.Format("{0}.{1}", splittedInput[0], splittedInput[1]));
+                    var culture = (CultureInfo)CultureInfo.CurrentCulture.Clone();
+                    culture.NumberFormat.NumberDecimalSeparator = ".";
+                    float inputNum = float.Parse(input,culture);
+                    MessageBox.Show(Convert.ToString(inputNum));
+                    
                     string ToReturnStr = string.Format("{0}", (((float)Math.Truncate((inputNum * 255) * 10000)) / 10000));
+                    ToReturnStr = ToReturnStr.Replace(',','.');
+                    MessageBox.Show(ToReturnStr);
                     return ToReturnStr;
                 } else
                 {
@@ -59,52 +66,46 @@ namespace RgbConverter
                 string secondNumber = toConvert_words[1];
                 string thirdNumber = toConvert_words[2];
                 string fourthNumber = toConvert_words[3];
-                if ((float.Parse(firstNumber) > 1.001 || float.Parse(secondNumber) > 1.001 || float.Parse(thirdNumber) > 1.001 || float.Parse(fourthNumber) > 1.001) && inverse_check.Checked)
+                if (inverse_check.Checked)
                 {
-                    MessageBox.Show("The numbers you're trying to convert are greater then 1, if you'd like to convert from 255 to 1 format please uncheck the reverse checkbox");
+                    firstNumber = freex_truncate(firstNumber, 1);
+                    secondNumber = freex_truncate(secondNumber, 1);
+                    thirdNumber = freex_truncate(thirdNumber, 1);
+                    fourthNumber = freex_truncate(fourthNumber, 1);
+                    if (!(firstNumber == "error" || secondNumber == "error" || thirdNumber == "error" || fourthNumber == "error")) {
+                            converted_text.Text = string.Format("{0},{1},{2},{3}", firstNumber, secondNumber, thirdNumber, fourthNumber);
+                    } else
+                    {
+                        converted_text.Text = "Syntax Error";
+                    }
+
                 } else
                 {
-
-                    if (inverse_check.Checked)
+                    firstNumber = freex_truncate(firstNumber, 255);
+                    secondNumber = freex_truncate(secondNumber, 255);
+                    thirdNumber = freex_truncate(thirdNumber, 255);
+                    fourthNumber = freex_truncate(fourthNumber, 255);
+                    if (!noArma3_check.Checked)
                     {
-                        firstNumber = freex_truncate(firstNumber, 1);
-                        secondNumber = freex_truncate(secondNumber, 1);
-                        thirdNumber = freex_truncate(thirdNumber, 1);
-                        fourthNumber = freex_truncate(fourthNumber, 1);
-                        if (!(firstNumber == "error" || secondNumber == "error" || thirdNumber == "error" || fourthNumber == "error")) {
-                                converted_text.Text = string.Format("{0},{1},{2},{3}", firstNumber, secondNumber, thirdNumber, fourthNumber);
+                        if (!(firstNumber == "error" || secondNumber == "error" || thirdNumber == "error" || fourthNumber == "error"))
+                        {
+                            converted_text.Text = string.Format("#(rgb,8,8,3)color({0},{1},{2},{3});", firstNumber, secondNumber, thirdNumber, fourthNumber);
                         } else
                         {
                             converted_text.Text = "Syntax Error";
                         }
-
-                    } else
+                    }
+                    else
                     {
-                        firstNumber = freex_truncate(firstNumber, 255);
-                        secondNumber = freex_truncate(secondNumber, 255);
-                        thirdNumber = freex_truncate(thirdNumber, 255);
-                        fourthNumber = freex_truncate(fourthNumber, 255);
-                        if (!noArma3_check.Checked)
+                        if (!(firstNumber == "error" || secondNumber == "error" || thirdNumber == "error" || fourthNumber == "error"))
                         {
-                            if (!(firstNumber == "error" || secondNumber == "error" || thirdNumber == "error" || fourthNumber == "error"))
-                            {
-                                converted_text.Text = string.Format("#(rgb,8,8,3)color({0},{1},{2},{3});", firstNumber, secondNumber, thirdNumber, fourthNumber);
-                            } else
-                            {
-                                converted_text.Text = "Syntax Error";
-                            }
-                        }
-                        else
+                            converted_text.Text = string.Format("{0},{1},{2},{3}", firstNumber, secondNumber, thirdNumber, fourthNumber);
+                        } else
                         {
-                            if (!(firstNumber == "error" || secondNumber == "error" || thirdNumber == "error" || fourthNumber == "error"))
-                            {
-                                converted_text.Text = string.Format("{0},{1},{2},{3}", firstNumber, secondNumber, thirdNumber, fourthNumber);
-                            } else
-                            {
-                                converted_text.Text = "Syntax Error";
-                            }
+                            converted_text.Text = "Syntax Error";
                         }
-                }   }
+                    }
+                }
                 
             }
         }
